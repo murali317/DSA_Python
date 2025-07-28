e1c8780d9656681727334fff831721e6d11e70e9
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2025-07-28 18:50:39 +0530

    Time: 549 ms (72.32%) | Memory: 20.6 MB (69.21%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,13 +1,16 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # time - O(n^2) space - O(N)
+        # INTUITION - unlike 2sum, no need of hashmap here as we have to output elements directly, not its indices. so take 2 pointers l & r at 2nd position and last positions. keep checking those 2 pointers with first element. dont forget to skip duplicates by checking if current element (l) == previous element (nums[l-1]).
+        # time - O(n^2)
+        # space - O(n)
+        
         nums.sort() # O(n log n)
         res = [] # O(n) for storing triplets in worst case
         for i,j in enumerate(nums): # O(n)
-            if i > 0 and j == nums[i-1]:
+            if i > 0 and j == nums[i-1]: #l this is for the outer loop, not inner loop (not inner 2 elements with pointers l & r) i.e, for i = 1 & i = 2, j is -1 in sorted array which is duplicate. it is skipped as it may lead to have 2 [-1,0,-1] if processed. Finally it avoids duplicate triplet startings.
                 continue
             l, r = i + 1, len(nums) - 1
-            while l < r: # O(n)
+            while l < r: # O(n) per i
                 three_sum = j + nums[l] + nums[r]
                 if three_sum > 0:
                     r -= 1
@@ -19,3 +22,4 @@ class Solution:
                     while l < r and nums[l] == nums[l-1]:
                         l += 1
         return res
+

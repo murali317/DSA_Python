d272ca3094b066732c5c5b156f9bf38309297aef
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2025-07-28 16:58:04 +0530

    Time: 7 ms (23.32%) | Memory: 18.5 MB (59.47%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -7,29 +7,17 @@ class Solution:
         #     comp = target - j
         #     if comp in hashmap:
         #         return [hashmap[comp]+1, i+1]
-        #     hashmap[j] = i
-
-
-        l, r = 0, len(numbers) - 1
-        while l<r:
-            s = numbers[l] + numbers[r]
-            if s == target:
-                return [l+1, r+1]
-            elif s < target:
-                l += 1
-            else:
-                r -= 1
-        
+        #     hashmap[j] = i        
 
         # using 2 pointers (like binary search but not, as we are not diving in half just traversing) is better as the input array is sorted.
         # time - O(n) space - O(1)
-        # left, right = 0, len(numbers)-1
-        # while left < right:
-        #     s = numbers[left] + numbers[right]
-        #     if s == target:
-        #         return [left + 1, right + 1] #space - O(1) since its fixed
-        #     elif s < target:
-        #         left += 1
-        #     else:
-        #         right -= 1
-        # return [left + 1, right + 1]
+        left, right = 0, len(numbers)-1
+        while left < right:
+            s = numbers[left] + numbers[right]
+            if s == target:
+                return [left + 1, right + 1] #space - O(1) since its fixed
+            elif s < target:
+                left += 1
+            else:
+                right -= 1
+        return [left + 1, right + 1] # this is not needed as it passess every test case.

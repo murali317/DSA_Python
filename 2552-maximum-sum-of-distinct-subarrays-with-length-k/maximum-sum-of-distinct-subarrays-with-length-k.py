d21e544997a1ad2e7492ae9a2bbeab6f4566c8c1
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2025-07-29 23:30:31 +0530

    Time: 92 ms (85.69%) | Memory: 34.7 MB (66.65%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,38 +1,41 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         # time - O(n)
-        # space - O(k) not O(n) because set is storing at most k unique elements, not n
+        # space - O(k) not O(n) because set is storing at most k unique elements, not all n
         # if the size grows till the length of array, the might be O(n) which is not the case in this problem.
-        # l,s_sum,max_sum = 0,0,float(-inf)
-        # s = set()
-        # for r in range(len(nums)):
-        #     while nums[r] in s:
-        #         s.remove(nums[l])
-        #         s_sum -= nums[l]
-        #         l += 1
-        #     s.add(nums[r])
-        #     s_sum += nums[r]
-        #     if r-l+1 == k:
-        #         max_sum = max(max_sum, s_sum)
-        #         s.remove(nums[l])
-        #         s_sum -= nums[l]
-        #         l += 1
-        # return max_sum if max_sum != float(-inf) else 0
+        l,s_sum,max_sum = 0,0,float(-inf)
+        s = set()
+        for r in range(len(nums)):
+            while nums[r] in s: # have to remove until that duplicate is present and not O(n^2) since it just moves foward and doesn't reprocess old elements. - amortized
+                s.remove(nums[l]) # O(1)
+                s_sum -= nums[l]
+                l += 1
+            s.add(nums[r]) # O(1)
+            s_sum += nums[r]
+            if r-l+1 == k:
+                max_sum = max(max_sum, s_sum)
+                s.remove(nums[l])
+                s_sum -= nums[l]
+                l += 1
+        return max_sum if max_sum != float(-inf) else 0
 
-        l, s_sum, m_sum, s = 0, 0, float(-inf), set()
-        for i in range(len(nums)):
-            while nums[i] in s:
-                s.remove(nums[l])
-                s_sum -= nums[l]
-                l += 1
-            s.add(nums[i])
-            s_sum += nums[i]
-            if i-l+1 == k:
-                m_sum = max(s_sum, m_sum)
-                s_sum -= nums[l]
-                s.remove(nums[l])
-                l += 1
-        return m_sum if m_sum != float(-inf) else 0
+        # time - O(n)
+        # space - O(k)
+        start = 0; res = float(-inf); total = 0; d = defaultdict(int)
+        for end in range(len(nums)):
+            total += nums[end]
+            d[nums[end]] += 1
+            while d[nums[end]] > 1: # use while instead of 'if' to filter duplicates until it becomes duplicates-free.
+                total -= nums[start]
+                d[nums[start]] -= 1
+                start += 1
+            if (end - start + 1) == k:
+                res = max(res, total)
+                total -= nums[start]
+                d[nums[start]] -= 1
+                start += 1
+            
+        return res if res != float(-inf) else 0
 
         
         

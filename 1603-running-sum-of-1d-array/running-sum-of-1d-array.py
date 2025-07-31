74d4379de72b9a46bbe0a1b4785da1bed9cf5f69
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2025-07-31 20:37:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (70.40%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -7,6 +7,18 @@ class Solution:
         #     arr.append(sum(nums[:i+1]))
         # return arr
 
+        # INTUITION - keep summing it cumulatively and do inplace modification
+        # time - O(n)
+        # space - O(1)
         for i in range(1, len(nums)):
             nums[i] = nums[i] + nums[i-1]
+        return nums
+
+        # ALTERNATE APPRAOCH
+        # time - O(n)
+        # space - O(1)
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i]
+            nums[i] = total
         return nums

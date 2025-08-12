989e399fcb4265cb55624013bd219b248541bd0d
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2025-08-12 17:51:48 +0530

    Time: 170 ms (32.79%) | Memory: 19 MB (28.58%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,5 +1,7 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # INTUITION - use binary search by starting low = 1 and high = max(piles). Then keep increasing/decreasing mid value upon comparing with 'h'.
+        
         # time - O(n log n)
         # space - O(1)
 
@@ -9,9 +11,9 @@ class Solution:
         count = 0
         while low < high:
             mid = (low + high) // 2 
-            count = sum(math.ceil(i/mid) for i in piles) # Count needs to represent only this iteration’s total hours — not a running total (so its not count += ).
+            count = sum(math.ceil(i/mid) for i in piles) # Count needs to represent only this iteration’s total hours — not a running total (so its not count +=).
             if count <= h:
                 high = mid
             else:
                 low = mid + 1
-        return high
+        return high # take an example and check. dont return count.

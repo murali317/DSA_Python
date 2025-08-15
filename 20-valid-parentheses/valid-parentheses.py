a05a69abeaedf50369e1fdd5ac760f8c4d8be281
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2025-08-15 13:14:20 +0530

    Time: 3 ms (22.28%) | Memory: 17.7 MB (96.42%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # INTUITION - Take a hashmap of closed -> open brackets. Iterate over 's', compare the items with previously added open brackets in stack by popping them and return.
+        # Time - O(n) - we iterate over the string once.
+        # Space - O(n) - could be O(k) which is no of opening brackets in 's', but in W.C., all brackets of 's' could be open.
+        
+        stack = []
+        hashing = {')': '(', '}': '{', ']': '['}
+        for i in s:
+            if i in hashing:
+                top_ele = stack.pop() if stack else '#' # it means when any bracket compared with '#' it automatically returns False as they cant be same. 
+                if hashing[i] != top_ele:
+                    return False
+            else:
+                stack.append(i) # appending the opening brackets.
+        return not stack # if empty is stack means we have processed all the open brackets so True. Otherwise False
+                
+            
+
+

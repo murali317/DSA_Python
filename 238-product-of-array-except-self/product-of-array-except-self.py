1b4be3a75fb6e0380719165c3848728821f13650
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2025-09-07 09:59:58 +0530

    Time: 31 ms (33.00%) | Memory: 27.2 MB (5.29%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,24 +1,9 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         # INTUITION - Take an output array & a prefix = 1 -> as we iterate in nums, for the left side of each index, store it in output[i] and consecutively mulitply prefix with nums[i]. Similarly, take a postfix = 1 -> as we iterate from the end, for the right side of each index, store (postfix * output[i]) in output[i] and consecutively mulitply postfix with nums[i].
-        # time - O(n) - for looping
-        # space - O(1)
 
-        output = [1] * len(nums) # O(1) space as mentioned in problem
-        
-        prefix = 1 # prod of all left side elements of current index
-        for i in range(len(nums)):
-            output[i] = prefix
-            prefix *= nums[i]
-        
-        postfix = 1 # prod of all right side elements of current index
-        for i in range(len(nums)-1, -1, -1):
-            output[i] *= postfix
-            postfix *= nums[i]
-        
-        return output
-
-        # ALTERNATE SOLUTION ---------------------------------
+        # time - O(n)
+        # space - O(n)
         n = len(nums)
         prefix = [1] * n
         suffix = [1] * n
@@ -34,3 +19,21 @@ class Solution:
             result[i] = prefix[i] * suffix[i]
 
         return result
+
+# ---------------------------------------------------------------------
+
+        # time - O(n)
+        # space - O(1)
+
+        output = [1] * len(nums) # O(1) space as mentioned in problem
+        prefix = 1 # prod of all left side elements of current index
+        for i in range(len(nums)):
+            output[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1 # prod of all right side elements of current index
+        for i in range(len(nums)-1, -1, -1):
+            output[i] *= postfix
+            postfix *= nums[i]
+        
+        return output        

10022d539e74c6966982be81e4d32c671c41e396
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2025-09-07 09:58:28 +0530

    Time: 26 ms (48.34%) | Memory: 23.5 MB (41.04%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -18,3 +18,19 @@ class Solution:
         
         return output
 
+        # ALTERNATE SOLUTION ---------------------------------
+        n = len(nums)
+        prefix = [1] * n
+        suffix = [1] * n
+        result = [1] * n
+
+        for i in range(1, n):
+            prefix[i] = prefix[i-1] * nums[i-1]
+
+        for i in range(n-2, -1, -1):
+            suffix[i] = suffix[i+1] * nums[i+1]
+
+        for i in range(n):
+            result[i] = prefix[i] * suffix[i]
+
+        return result

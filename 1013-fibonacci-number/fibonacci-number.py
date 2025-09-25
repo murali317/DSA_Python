66c88cfcc8b46d2aa4215a68d5387fa7a2cc00da
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2025-09-25 17:57:29 +0530

    Time: 599 ms (5.11%) | Memory: 17.9 MB (23.50%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -6,4 +6,38 @@ class Solution:
         if n <= 1:
             return n
         return self.fib(n-1) + self.fib(n-2)
+
+        # MEMOIZATION -----------
+        # time - O(n) # Each unique value of n is computed only once and stored in memo.
+        # space - O(n) - 1. memo dictionary 2. Recursion call stack
+
+        memo = {0:0, 1:1}
+        def helper(n):
+            if n <= 1:
+                return n
+            if n in memo:
+                return memo[n]
+            memo[n] = helper(n-1) + helper(n-2)
+            return memo[n]
+        return helper(n)
+
+        # TABULATION ---------------
+        # time - O(n)
+        # space - O(n)
+
+        dp = [0] * (n+1)
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+        # SPACE-OPTIMIZED ----------------
+        # time - O(n)
+        # space - O(1)
+
+        prev1, prev2 = 1, 0
+        for i in range(2, n+1):
+            curr = prev1 + prev2
+            prev2, prev1 = prev1, curr
+        return prev1
         

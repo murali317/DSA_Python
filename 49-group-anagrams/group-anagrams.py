fdfdea4de284fbeeae780a82ff22ee9c223018c2
Devarakonda Muralikrishna <67271496+murali317@users.noreply.github.com>
2025-12-25 17:21:56 +0530

    Time: 15 ms (49.75%) | Memory: 20.1 MB (97.06%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,21 +1,51 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         from collections import defaultdict
-        # INTUITION - group the strs as key is sorted value(s) and its value is normal elements (before sorting). Then return the values.
+        # INTUITION - group the strs as key is sorted value(s) and its value is normal elements (before sorting). Then return the values as a list.
         # time - O(n.k log k)
         # space - O(n.k)
 
-        hash = defaultdict(list)
-        arr = []
-        for i in strs: # O(n)
-            j = ''.join(sorted(i)) # O(k log k) time k is length of each word, O(n.k) space
-            # if j in hash: # this part is not required as it is defaultdict(list), by default 0 only.
-            #     hash[j].append(i)
-            # else:
-            #     hash[j] = [i]
-            hash[j].append(i)
+        # hash = defaultdict(list)
+        # arr = [] 
+        # for i in strs: # O(n)
+        #     j = ''.join(sorted(i)) # O(k log k) k is length of each word, O(n.k) space
+        #     # if j in hash: # this part is not required as it is defaultdict(list), by default 0 only.
+        #     #     hash[j].append(i)
+        #     # else:
+        #     #     hash[j] = [i]
+        #     hash[j].append(i)
 
-        # for i in hash.values():
-        #     arr.append(i)
-        # return arr
-        return list(hash.values()) # O(n) time for flattening, 
+        # # for i in hash.values():
+        # #     arr.append(i)
+        # # return arr
+        # return list(hash.values()) # O(n) time for flattening
+
+
+        hash = defaultdict(list) 
+        res = []
+        for i in range(len(strs)):
+            s = ''.join(sorted(strs[i]))
+            hash[s].append(strs[i])
+        
+        for i in hash:
+            res.append(hash[i])
+        
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

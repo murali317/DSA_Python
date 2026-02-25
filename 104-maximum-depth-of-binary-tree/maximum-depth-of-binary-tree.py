54edb9d86691556caa1a2f6cba0ba27c59f7d7e3
Muralikrishna Devarakonda <67271496+murali317@users.noreply.github.com>
2026-02-25 18:21:16 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (21.83%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -24,8 +24,13 @@ class Solution:
         #     c += 1 # here we are counting the levels.
         # return c # root = [3,9,20,null,null,15,7] Output: 3
 
+        # RECURSIVE APPROACH
+        # time - O(n)
+        # space - O(h) as it winds up from the bottom, we consider height & logn for balanced, n for skewed.
         if not root:
             return 0
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
         return 1 + max(left, right)
+
+        # for height at each node = 1+max(height of lst, height of rst)

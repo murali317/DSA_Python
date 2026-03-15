ad89652ad52f395c1d8cc0fd1ff78d69195d9055
Muralikrishna Devarakonda <67271496+murali317@users.noreply.github.com>
2026-03-15 17:12:07 +0530

    Time: 6 ms (46.70%) | Memory: 19.2 MB (96.81%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -4,11 +4,27 @@ class Solution:
         # time - O(n)
         # space - O(1)
 
-        d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        total = 0
-        for i in range(len(s) - 1):
-            if d[s[i]] < d[s[i+1]]:
-                total -= d[s[i]]
+        # d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        # total = 0
+        # for i in range(len(s) - 1):
+        #     if d[s[i]] < d[s[i+1]]:
+        #         total -= d[s[i]]
+        #     else:
+        #         total += d[s[i]]
+        # return total + d[s[-1]] # don't skipping the final character's value 1994
+
+        # time - O(n)
+        # space - O(1)
+        
+        hash = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D':500, 'M': 1000}
+        curr = 0
+        i = 0
+        while i < len(s):
+            if i+1 < len(s) and hash[s[i]] < hash[s[i+1]]:
+                curr += hash[s[i+1]] - hash[s[i]]
+                i += 2
             else:
-                total += d[s[i]]
-        return total + d[s[-1]] # don't skipping the final character's value
+                curr += hash[s[i]]
+                i += 1
+
+        return curr

1b99289bfdc2782560e87cc618d2133cc5432bf6
Muralikrishna Devarakonda <67271496+murali317@users.noreply.github.com>
2026-03-16 17:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (87.59%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -3,37 +3,15 @@ class Solution:
         # INTUITION - First turn this list to a max-heap by negating the values, pop 1st & 2nd largest of them, subtract and push it to the heap until only 1 element is left. In btw, if both 1st&2nd largest are equal, continue (dont bother as they will be popped and not pushed).
         # time - O(n log n) # O(n) + O(n) + O(n log n)
         # space - O(n)
+        
         import heapq
         if not stones:
             return None
-        # heap = [-x for x in stones] # negate the values [-2,-7,-4,-1,-8,-1] # O(n)
-        # heapq.heapify(heap) # this converts list to a heap in-place.
-        # while len(heap) > 1: # O(n)
-        #     first = -heapq.heappop(heap) # largest # O(log n)
-        #     second = -heapq.heappop(heap) # second-largest # O(log n)
-        #     if first != second:
-        #         heapq.heappush(heap, -(first-second)) # we have to push negate values  to heap otherwise smallest values would start coming out.
-        # return -heap[0] if heap else 0
-
-        arr = [-x for x in stones]
-        heapq.heapify(arr)
-        while len(arr) > 1:
-            x = -heapq.heappop(arr)
-            y = -heapq.heappop(arr)
-            if x!=y:
-                heapq.heappush(arr, -abs(x-y))
-        return -arr[0] if arr else 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        heap = [-x for x in stones] # negate the values [-2,-7,-4,-1,-8,-1] # O(n)
+        heapq.heapify(heap) # this converts list to a heap in-place.
+        while len(heap) > 1: # O(n)
+            first = -heapq.heappop(heap) # largest # O(log n)
+            second = -heapq.heappop(heap) # second-largest # O(log n)
+            if first != second:
+                heapq.heappush(heap, -abs(first-second)) # we have to push negate values  to heap otherwise smallest values would start coming out.
+        return -heap[0] if heap else 0
